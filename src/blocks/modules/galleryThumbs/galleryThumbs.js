6ba9c0e5df037b32937d8bf11439023f0e4043eb
kosmos <3apa3a.iickov@gmail.com>
2020-04-12 14:14:01 +0300

    add ps add obhod

--- src/blocks/modules/galleryThumbs/galleryThumbs.js
+++ src/blocks/modules/galleryThumbs/galleryThumbs.js
@@ -1,48 +1,62 @@
 import Swiper from 'swiper/js/swiper';
 import 'bootstrap/js/dist/modal';
 
+
+function initThumbsSlider() {
+    let $slider = $('.thumbs-slider .thumbs-slider__main');
+    console.log($slider.length);
+
+    $slider.each(function () {
+        let paginationSlider = $(this).parent().find('.swiper-pagination')[0],
+            nextButton = $(this).parent().find('.swiper-button-next'),
+            prevButton = $(this).parent().find('.swiper-button-prev'),
+            paginationSliderThumbs = $(this).find('.thumbs-slider__nav')[0],
+            slideHeight = $(this).children('.swiper-wrapper').find('.swiper-slide .thumbs-slider__main-img').height(),
+            sliderButtonPositionY = parseInt(slideHeight / 2);
+
+
+
+
+
+        let galleryThumbs = new Swiper(this, {
+            slidesPerView: 1,
+            pagination: {
+                el: paginationSlider,
+                type: 'fraction',
+            },
+            navigation: {
+                nextEl: nextButton[0],
+                prevEl: prevButton[0],
+            },
+            thumbs: {
+                swiper: {
+                    el: paginationSliderThumbs,
+                    slidesPerView: 7,
+                    spaceBetween: 8,
+                }
+            },
+            autoHeight: true,
+            on: {
+                init: function () {
+                    nextButton.css('top', sliderButtonPositionY);
+                    // prevButton.css('top', sliderButtonPositionY);
+                },
+            },
+        });
+    });
+}
+
 $(document).ready(function () {
     if ($('.thumbs-slider').length > 0) {
-
-        let $slider = $('.thumbs-slider .thumbs-slider__main');
-
-        $slider.each(function () {
-            let paginationSlider = $(this).parent().find('.swiper-pagination')[0],
-                nextButton = $(this).parent().find('.swiper-button-next')[0],
-                prevButton = $(this).parent().find('.swiper-button-prev')[0],
-                paginationSliderThumbs =  $(this).find('.thumbs-slider__nav')[0],
-                slideHeight = $(this) .children('.swiper-wrapper').find('.swiper-slide .thumbs-slider__main-img').height(),
-                sliderButton = $(this).parent().find('.swiper-button'),
-                sliderButtonPositionY = parseInt(slideHeight / 2);
-
-
-            let galleryThumbs = new Swiper(this, {
-                slidesPerView: 1,
-                pagination: {
-                    el: paginationSlider,
-                    type: 'fraction',
-                },
-                navigation: {
-                    nextEl:  nextButton,
-                    prevEl:  prevButton ,
-                },
-                thumbs: {
-                    swiper : {
-                        el :  paginationSliderThumbs,
-                        slidesPerView: 7,
-                        spaceBetween : 8,
-                    }
-                },
-                on: {
-                    init: function () {
-                        sliderButton.css('top', sliderButtonPositionY)
-                    },
-                },
-            });
-        });
+        initThumbsSlider()
     }
-})
-
-
+});
+$('body').on('click', '.thumbs-slider__zoom', function (e) {
+    e.preventDefault();
+    $('#gallery_popup').modal('show');
+});
+$('#gallery_popup').on('shown.bs.modal', function () {
+    initThumbsSlider()
+});
 
 
---
+++ src/blocks/modules/practices/practices__widget.js
@@ -0,0 +1,13 @@
+modules.define('practices__widget', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declElem('practices', 'widget', {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
