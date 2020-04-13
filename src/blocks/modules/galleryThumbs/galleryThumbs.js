f48fef6211850041bd9d5bb57e5da09b5d5414b7
Ilya Shmanay <ilya21743@gmail.com>
2020-04-13 21:24:21 +0300

    text page buttons position top fix (only document ready for now).

--- src/blocks/modules/galleryThumbs/galleryThumbs.js
+++ src/blocks/modules/galleryThumbs/galleryThumbs.js
@@ -1,21 +1,37 @@
 import Swiper from 'swiper/js/swiper';
 import 'bootstrap/js/dist/modal';
 
+function setSlidersButtonsPositionTop() {
+    var sliders = $('.js-thumbs-slider__main');
+
+    sliders.each(function(){
+        let thisSlideHeight = $(this).find('.js-thumbs-slider__main-img').eq(0).outerHeight(),
+            thisBtnHeight = $(this).parent().find('.swiper-button').eq(0).outerHeight(),
+            thisButtonsPositionTop = thisSlideHeight/2 - thisBtnHeight/2 + 'px';
+        console.log(thisSlideHeight);
+        console.log(thisBtnHeight);
+        console.log(thisButtonsPositionTop);
+
+        $(this).parent().find('.swiper-button').css({
+            'top': thisButtonsPositionTop
+        })
+    }); 
+}
+
+$(document).ready(function () {
+    setSlidersButtonsPositionTop();
+})
 
 function initThumbsSlider() {
     let $slider = $('.thumbs-slider .thumbs-slider__main');
-    console.log($slider.length);
 
     $slider.each(function () {
         let paginationSlider = $(this).parent().find('.swiper-pagination')[0],
             nextButton = $(this).parent().find('.swiper-button-next'),
             prevButton = $(this).parent().find('.swiper-button-prev'),
-            paginationSliderThumbs = $(this).find('.thumbs-slider__nav')[0],
-            slideHeight = $(this).children('.swiper-wrapper').find('.swiper-slide .thumbs-slider__main-img').height(),
-            sliderButtonPositionY = parseInt(slideHeight / 2);
+            paginationSliderThumbs = $(this).find('.thumbs-slider__nav')[0];
 
-
-
+            //console.log(buttonHeight);
 
 
         let galleryThumbs = new Swiper(this, {
@@ -38,7 +54,13 @@ function initThumbsSlider() {
             autoHeight: true,
             on: {
                 init: function () {
-                    nextButton.css('top', sliderButtonPositionY);
+                    // var slideHeight = $($(this)[0].$el).parent().find('img:eq(0)').outerHeight(),
+                    //     buttonHeight = $($(this)[0].$el).parent().find('.swiper-button:eq(0)').outerHeight(),
+                    //     sliderButtonPositionY = parseInt(slideHeight/2 - buttonHeight/2);
+
+                    // console.log(slideHeight);
+                    // console.log(buttonHeight);
+                    // nextButton.css('top', sliderButtonPositionY);
                     // prevButton.css('top', sliderButtonPositionY);
                 },
             },
