341c4c5d12e5bfe1c65e165264a48e80c44c4d2e
Ilya Shmanay <ilya21743@gmail.com>
2020-05-17 22:05:45 +0300

    adaptive informations systems

--- src/blocks/modules/information-systems-search/information-systems-search.js
+++ src/blocks/modules/information-systems-search/information-systems-search.js
@@ -1,33 +1,58 @@
 import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar';
 import 'jquery-mousewheel/jquery.mousewheel';
 
-$('body').on('click', function(e) {
-	var div = $(".js-search"); // тут указываем ID элемента
-	if (!div.is(e.target) // если клик был не по нашему блоку
-	    && div.has(e.target).length === 0) { // и не по его дочерним элементам
-		$('.js-search-dropdown').slideUp(400); // скрываем его
-	}
+$('body').on('click', function (e) {
+    var div = $(".js-search"); // тут указываем ID элемента
+    if (!div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+        $('.js-search-dropdown').slideUp(400); // скрываем его
+    }
 });
 
 $('.js-search-dropdown').mCustomScrollbar({
-	axis: "y",
-	scrollInertia: 400, 
-	scrollBarPosition: "insight"
+    axis: "y",
+    scrollInertia: 400,
+    scrollBarPosition: "insight"
 })
 
-$('body').on('focus', '.js-search__input', function() {
-	$('.js-search-dropdown').slideDown(400);
+$('body').on('focus', '.js-search__input', function () {
+    $('.js-search-dropdown').slideDown(400);
+    if (window.innerWidth <= 575) {
+        let searchPositionTop = $('.js-search').offset().top + 'px';
+        $('.js-search').addClass('mobile-active');
+        $('html, body').addClass('search-off-scroll');
+        $('html').animate({
+            scrollTop: searchPositionTop
+        }, 400);
+        setTimeout(function () {
+            $('.js-search-close').addClass('active');
+        }, 400);
+        $('.js-search__input').addClass('active');
+        $('.js-search-shade').addClass('active');
+    }
 });
 
-$('body').on('input', '.js-search__input', function() {
-	if($(this).val().length >= 2) {
-		console.log('Показываем результаты поиска');
-	} else {
-		console.log('Показываем блок "Вы недавно искали"');
-	}
+$('body').on('click', '.js-search-close', function () {
+    console.log('click');
+    if (window.innerWidth <= 575) {
+        $('.js-search').removeClass('mobile-active');
+        $('html, body').removeClass('search-off-scroll');
+        $('.js-search__input').removeClass('active');
+        $('.js-search-dropdown').slideUp(400);
+        $(this).removeClass('active');
+        $('.js-search-shade').removeClass('active');
+    }
+});
 
-	var searchValue = $(this).val();
-    $('.js-search-dropdown__link').each( function () {
+$('body').on('input', '.js-search__input', function () {
+    if ($(this).val().length >= 2) {
+        console.log('Показываем результаты поиска');
+    } else {
+        console.log('Показываем блок "Вы недавно искали"');
+    }
+
+    var searchValue = $(this).val();
+    $('.js-search-dropdown__link').each(function () {
         var resultValue = $(this).find('.text').text();
         var searchValueNew = new RegExp(searchValue, 'i');
         var fix = resultValue.replace(searchValueNew, (el) => {
@@ -36,4 +61,19 @@ $('body').on('input', '.js-search__input', function() {
         $(this).find('.text').html(fix);
     });
 
+});
+
+$(document).ready(function () {
+    if ($('.js-search').length > 0) {
+        $(window).resize(function () {
+            if (window.innerWidth > 575) {
+                $('.js-search').removeClass('mobile-active');
+                $('html, body').removeClass('search-off-scroll');
+                $('.js-search-close').removeClass('active');
+                $('.js-search-shade').removeClass('active');
+                $('.js-search__input').removeClass('active');
+                $('.js-search-dropdown').slideUp(400);
+            }
+        })
+    }
 });
