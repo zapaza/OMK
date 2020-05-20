fa5dc101bc2192b266a8697b3f8db16c136f461f
kosmos <stas@coffeestudio.ru>
2020-05-21 00:05:21 +0300

    ads pages

---
+++ src/blocks/modules/ads-slider-user/ads-slider-user.js
@@ -0,0 +1,58 @@
+import Swiper from 'swiper/js/swiper';
+import 'bootstrap/js/dist/modal';
+
+$(document).ready(function () {
+
+    if ($('.ads-user__photo-slider').length > 0){
+        let $this = $('.js-ads-user__slider-main'),
+            paginationSlider = $this.parent().find('.swiper-pagination')[0],
+            nextButton = $this.parent().find('.swiper-button-next'),
+            prevButton = $this.parent().find('.swiper-button-prev'),
+            paginationSliderThumbs = $this.find('.js-ads-user__slider-nav')[0];
+
+        let adsUserSlider = new Swiper ( $this , {
+            slidesPerView: 1,
+            pagination: {
+                el: paginationSlider,
+                type: 'fraction',
+            },
+            navigation: {
+                nextEl: nextButton[0],
+                prevEl: prevButton[0],
+            },
+            breakpoints:{
+                100 :{
+                    slidesPerView: 'auto',
+                    spaceBetween: 8,
+
+
+                },
+                1024 : {
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                }
+
+            },
+            thumbs: {
+                swiper: {
+                    el:  paginationSliderThumbs,
+
+                    spaceBetween: 8,
+                    breakpoints:{
+                        1279 :{
+                            slidesPerView: 7,
+                        },
+                        1023 : {
+                            slidesPerView: 6,
+                        }
+
+                    }
+                }
+            },
+
+        });
+    }
+
+
+
+});
