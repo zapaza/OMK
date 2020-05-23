14a32ad87baf6f8dcb51e384e1b5e9d0c2d6fab7
Ilya Shmanay <ilya21743@gmail.com>
2020-05-23 20:34:43 +0300

    fixes after review

--- src/blocks/modules/regards-modal/regards-modal.js
+++ src/blocks/modules/regards-modal/regards-modal.js
@@ -1,3 +1,13 @@
 $('body').on('click', '.js-regards__all', function () {
     $('#regards_modal').modal('show');
 });
+
+$('#regards_modal').scroll(function () {
+    if(window.matchMedia('max-width: 575px')) {
+        if($('#regards_modal').scrollTop() > 0) {
+            $('#regards_modal .js-regards-title').addClass('scroll');
+        } else {
+            $('#regards_modal .js-regards-title').removeClass('scroll');
+        }
+    }
+});
